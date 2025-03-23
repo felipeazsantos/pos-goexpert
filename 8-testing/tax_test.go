e909e8cef8d385e47d252d82294c28e0450caf78
felipeazsantos <felipeasantos10@hotmail.com>
2025-03-23 16:03:18 -0300

    fuzzing and benchmarking tests

--- 8-testing/tax_test.go
+++ 8-testing/tax_test.go
@@ -2,7 +2,7 @@ package tax
 
 import "testing"
 
-func TestCalculateText(t *testing.T) {
+func TestCalculateTax(t *testing.T) {
 	amount := 500.0
 	expected := 5.0
 
@@ -10,5 +10,48 @@ func TestCalculateText(t *testing.T) {
 	if result != expected {
 		t.Errorf("Expected %f but got %f", expected, result)
 	}
+}
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type calcTax struct {
+		amount, expected float64
+	}
+
+	table := []calcTax{
+		{500.0, 5.0},
+		{1000.0, 10.0},
+		{1500.0, 10.0},
+	}
+
+		for _, item := range table {
+			result := CalculateTax(item.amount)
+			if result != item.expected {
+			t.Errorf("Excepted %f but got %f", item.expected, result)
+		}
+	}
+}
+
+func BenchmarkCalculateTest(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax(500.0)
+	}
+}
+func BenchmarkCalculate2Test(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax2(500.0)
+	}
+}
+
+func FuzzCalculateTax(f *testing.F) {
+	seed := []float64{-1, -2, -2.5, 500.0, 1000.0, 1500.0}
+	for _, amount := range seed {
+		f.Add(amount)
+	}
+	f.Fuzz(func(t *testing.T, amount float64) {
+		result := CalculateTax(amount)
+		if amount <= 0 && result != 0 {
+			t.Errorf("Received %f but expected 0", result)
+		}
+	})
 
 }
