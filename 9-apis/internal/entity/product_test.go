a157916ee7a0fd15e631dfe357a5625385c0d118
felipeazsantos <felipeasantos10@hotmail.com>
2025-04-13 13:52:05 -0300

    fixing tests

--- 9-apis/internal/entity/product_test.go
+++ 9-apis/internal/entity/product_test.go
@@ -1,50 +1,51 @@
 package entity
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewProduct(t *testing.T) {
-	product, err := NewProduct("Product 1", 10)
+	product, err := NewProduct("Product 1", "Description 1", 10)
 	assert.NoError(t, err)
 	assert.NotNil(t, product)
 	assert.NotEmpty(t, product.ID)
 	assert.Equal(t, "Product 1", product.Name)
+	assert.Equal(t, "Description 1", product.Description)
 	assert.Equal(t, 10, product.Price)
 }
 
 func TestProductValidate(t *testing.T) {
-	product, err := NewProduct("", 10)
+	product, err := NewProduct("", "Description 1", 10)
 	assert.Error(t, err)
 	assert.Nil(t, product)
+	assert.Equal(t, ErrNameIsRequired.Error(), err.Error())
 }
 
 func TestProductWhenNameIsEmpty(t *testing.T) {
-	product, err := NewProduct("", 10)
-	fmt.Println(err.Error(), ErrNameIsRequired.Error())
+	product, err := NewProduct("", "Description 1", 10)
 	assert.Error(t, err)
 	assert.Nil(t, product)
 	assert.Equal(t, ErrNameIsRequired.Error(), err.Error())
 }
 
 func TestProductValidateInvalidPrice(t *testing.T) {
-	product, err := NewProduct("Product 1", -1)
+	product, err := NewProduct("Product 1", "Description 1", -1)
 	assert.Error(t, err)
 	assert.Nil(t, product)
+	assert.Equal(t, ErrInvalidPrice.Error(), err.Error())
 }
 
 func TestProductWhenPriceIsZero(t *testing.T) {
-	product, err := NewProduct("Product 1", 0)
+	product, err := NewProduct("Product 1", "Description 1", 0)
 	assert.Error(t, err)
 	assert.Nil(t, product)
 	assert.Equal(t, ErrPriceIsRequired.Error(), err.Error())
 }
 
 func TestProductWhenPriceIsNegative(t *testing.T) {
-	product, err := NewProduct("Product 1", -1)
+	product, err := NewProduct("Product 1", "Description 1", -1)
 	assert.Error(t, err)
 	assert.Nil(t, product)
 	assert.Equal(t, ErrInvalidPrice.Error(), err.Error())
